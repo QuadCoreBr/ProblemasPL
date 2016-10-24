993cbc3f69ebfa40314cae5e9043608b7b780826
jdiazrdgz <jdiazrdgz@gmail.com>
2016-10-23 20:09:12 -0500

    starting the modulation of the views and controllers (principal container & welcome panel

---
+++ PLSolver/src/plsolver/controller/ControlSolverContainer.java
@@ -0,0 +1,13 @@
+package plsolver.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ControlSolverContainer implements ActionListener{
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ PLSolver/src/plsolver/controller/ControlWelcomePanel.java
@@ -0,0 +1,16 @@
+package plsolver.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import plsolver.view.WelcomePanel;
+
+public class ControlWelcomePanel implements ActionListener{
+    public ControlWelcomePanel(WelcomePanel panel){
+        
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
