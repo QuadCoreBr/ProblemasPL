ea71c50c0b6729ec70dd9a4dd639db2954da7d87
jdiazrdgz <jdiazrdgz@gmail.com>
2016-10-25 09:04:38 -0500

    llenado de funcion objetivo

--- PLSolver/src/plsolver/system/GestionadorFuncionObjetivo.java
+++ PLSolver/src/plsolver/system/GestionadorFuncionObjetivo.java
@@ -12,8 +12,11 @@ public class GestionadorFuncionObjetivo {
     
     public FuncionObjetivo crearFuncionObjetivo(String funcion){
         System.out.println(funcion);
-        if(validarFuncionObjetivo(funcion).equals("ok")){
+        String error=validarFuncionObjetivo(funcion);
+        if(error.equals("ok")){
             System.out.println("no esta vacia");
+            FuncionObjetivo fo=new FuncionObjetivo(listaVariables.size());
+            llenarFuncionObjetivo(fo);
             return fo;
         }else{
             return null;
@@ -42,7 +45,7 @@ public class GestionadorFuncionObjetivo {
             aux=st.nextToken();
             if(aux.equals("+")||aux.equals("-")){
                 ls.add(aux);
-                //System.out.println(aux+"es un signo");
+                System.out.println(aux+"es un signo");
                 this.listaSignos=ls;
             }else{
                 //es termino
@@ -52,12 +55,12 @@ public class GestionadorFuncionObjetivo {
                     aux2=st2.nextToken();
                     if(isNumeric(aux2)){
                         //es coeficiente
-                        //System.out.println(aux2+"es un coeficiente");
+                        System.out.println(aux2+"es un coeficiente");
                         lc.add(aux2);
                         this.listaCoeficientes=lc;
                     }else{
                         //es variable
-                        //System.out.println(aux2+"es unavariable");
+                        System.out.println(aux2+"es unavariable");
                         lv.add(aux2);
                         this.listaVariables=lv;
                     }
@@ -68,6 +71,43 @@ public class GestionadorFuncionObjetivo {
             return 1;
         }else{
             return 0;
+        }
+    }
+    public void llenarFuncionObjetivo(FuncionObjetivo fo){
+        switch(fo.getNoVariables()){
+            case 2:
+                fo.setC1(Integer.parseInt(listaCoeficientes.get(0)));
+                fo.setC2(Integer.parseInt(listaCoeficientes.get(1)));
+                fo.setS1(listaSignos.get(0));
+                fo.setV1(listaVariables.get(0));
+                fo.setV2(listaVariables.get(1));
+            break;
+            case 3:
+                fo.setC1(Integer.parseInt(listaCoeficientes.get(0)));
+                fo.setC2(Integer.parseInt(listaCoeficientes.get(1)));
+                fo.setC3(Integer.parseInt(listaCoeficientes.get(2)));
+                fo.setS1(listaSignos.get(0));
+                fo.setS2(listaSignos.get(1));
+                fo.setV1(listaVariables.get(0));
+                fo.setV2(listaVariables.get(1));
+                fo.setV2(listaVariables.get(2));
+            break;
+            case 4:
+                fo.setC1(Integer.parseInt(listaCoeficientes.get(0)));
+                fo.setC2(Integer.parseInt(listaCoeficientes.get(1)));
+                fo.setC3(Integer.parseInt(listaCoeficientes.get(2)));
+                fo.setC4(Integer.parseInt(listaCoeficientes.get(3)));
+                fo.setS1(listaSignos.get(0));
+                fo.setS2(listaSignos.get(1));
+                fo.setS3(listaSignos.get(2));
+                fo.setV1(listaVariables.get(0));
+                fo.setV2(listaVariables.get(1));
+                fo.setV2(listaVariables.get(2));
+                fo.setV2(listaVariables.get(3));
+            break;
+            default:
+                fo=null;
+            break;
         }
     }
     public static boolean isNumeric(String str) {
